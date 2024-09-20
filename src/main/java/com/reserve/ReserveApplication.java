c0f6bb83a9164303852b6a0123db579629237ea1
mwam0214 <mwam0214@outlook.jp>
2024-09-20 19:49:39 +0900

    Fix port binding

--- src/main/java/com/reserve/ReserveApplication.java
+++ src/main/java/com/reserve/ReserveApplication.java
@@ -2,6 +2,9 @@ package com.reserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReserveApplication {
@@ -9,5 +12,11 @@ public class ReserveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReserveApplication.class, args);
 	}
+	 @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.setPort(Integer.parseInt(System.getenv("PORT")));
+        return tomcat;
+    }
 
 }
